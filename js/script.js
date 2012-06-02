df63b92dd8ef7dba4be2d21db2a187312857e9b1
Gabriele Marchi <gabriele@thedoersproject.com>
2012-06-02 19:47:15 +0200

    new structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
 	$(".second").pageslide({ direction: "left" });
+	$(".first").pageslide({ direction: "right" });
+	
+
+		
+	$("#open_footer").toggle(
+	        function () {
+	            $("#footer").animate({marginBottom: 0}, 1000)
+	        },
+	        function () {
+	            $("#footer").animate({marginBottom: "-280px" }, 1000)
+	        }
+	    );
+	// fix sub nav on scroll
+	    var $win = $(window)
+	      , $nav = $('.subnav')
+	      , navTop = $('.subnav').length && $('.subnav').offset().top - 0
+	      , isFixed = 0
+	
+	    processScroll()
+	
+	    // hack sad times - holdover until rewrite for 2.1
+	    $nav.on('click', function () {
+	      if (!isFixed) setTimeout(function () {  $win.scrollTop($win.scrollTop() - 47) }, 10)
+	    })
+	
+	    $win.on('scroll', processScroll)
+	
+	    function processScroll() {
+	      var i, scrollTop = $win.scrollTop()
+	      if (scrollTop >= navTop && !isFixed) {
+	        isFixed = 1
+	        $nav.addClass('subnav-fixed')
+	      } else if (scrollTop <= navTop && isFixed) {
+	        isFixed = 0
+	        $nav.removeClass('subnav-fixed')
+	      }
+	    }
+	
 });
