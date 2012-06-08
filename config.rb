0b4ca6532bd9edf7f14903992738753ddf4f136c
Manuela Cammilleri <manuela.cammilleri@gmail.com>
2012-06-08 19:30:07 +0200

    blog single page lists

--- config.rb
+++ config.rb
@@ -16,10 +16,11 @@ fonts_dir = "fonts"
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 
-
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
+
+sass_options = {:debug_info=>true} # by Compass.app 
